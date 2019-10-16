1646c8d3e746f413a8bbf54ebf72fcc219ba0305
Anushek Prasal <anushekprasal@gmail.com>
2019-10-16 17:27:48 +0530

    sdm845-common: Remove OnePlus Doze

--- common.mk
+++ common.mk
@@ -101,9 +101,6 @@ PRODUCT_PACKAGES += \
     libvulkan \
     vendor.display.config@1.0
 
-# Doze
-PRODUCT_PACKAGES += \
-    OnePlusDoze
 
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
