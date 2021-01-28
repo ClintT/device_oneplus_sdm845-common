be30bd23d92eb1317f1c9f544fa32c043edc0c5b
chandu078 <chandudyavanapelli03@gmail.com>
2021-01-28 03:29:01 +0000

    [SQUASH]sdm845-common: Clean up! Revert "sdm845-common: init: workaround SafetyNet check"
    This reverts commit 9e7ccc47b1680601719d43e506f5eec73f0f5ba1.
    
    Revert "sdm845-common: Build signapk jar"
    
    This reverts commit 07708ccf6d0c1eef17d0d8c052bfd28e35824a03.
    
    Revert "sdm845-common: Add support for Pocket Judge by AOSPA"
    
    This reverts commit 288a2ac7e9aa305c5cb5c8b8c3e45bda4b13d7e8.
    
    * Move Camera provider service to configs/camera
    * Removepackages: Cleanup!
    
    Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := arcore CarrierSetup ConnMO DCMO Drive FM2 FilesPrebuilt GoogleTTS libqcomfm_jni obdm_stub qcom.fmradio Maps MyVerizonServices MaestroPrebuilt NfcNci NgaResources OnePlusCamera OBDM_Permissions PrebuiltGmail RecorderPrebuilt ScribePrebuilt Showcase SprintDM SprintHM SoundAmplifierPrebuilt SafetyHubPrebuilt TipsPrebuilt USCCDM VzwOmaTrigger VZWAPNLib YouTube YouTubeMusicPrebuilt
+LOCAL_OVERRIDES_PACKAGES := CarrierSetup ConnMO DCMO Drive FilesPrebuilt GoogleTTS libqcomfm_jni Maps NfcNci NgaResources OnePlusCamera PrebuiltGmail ScribePrebuilt SoundAmplifierPrebuilt SafetyHubPrebuilt TipsPrebuilt YouTube YouTubeMusicPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
